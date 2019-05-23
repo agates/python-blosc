f9e69d2164f435f774d1c2764568e9b99b7c5098
Alecks Gates <agates@mail.agates.io>
2019-05-23 18:24:59 -0500

    Clean up compare-pack-ptr

--- bench/compare-pack-ptr.py
+++ bench/compare-pack-ptr.py
@@ -52,18 +52,17 @@ for cname in blosc_cffi.compressor_list():
     print("\tCompr ratio: %.2f" % (in_.size*in_.dtype.itemsize*1. / len(c)))
 
     with ffi.from_buffer(in_) as in_ptr:
-        with ffi.from_buffer(out_) as out_ptr:
-            out_tmp = np.empty(in_.size, dtype=in_.dtype)
-            with ffi.from_buffer(out_tmp) as out_tmp_ptr:
-                ctic = timer()
-                c = blosc_cffi.compress_ptr(in_ptr,
-                                            in_.size, in_.dtype.itemsize,
-                                            clevel=clevel, shuffle=True, cname=cname)
-                ctoc = timer()
+        ctic = timer()
+        c = blosc_cffi.compress_ptr(in_ptr,
+                                    in_.size, in_.dtype.itemsize,
+                                    clevel=clevel, shuffle=True, cname=cname)
+        ctoc = timer()
 
-                dtic = timer()
-                blosc_cffi.decompress_ptr(c, out_tmp_ptr)
-                dtoc = timer()
+    out_tmp = np.empty(in_.size, dtype=in_.dtype)
+    with ffi.from_buffer(out_tmp) as out_tmp_ptr:
+        dtic = timer()
+        blosc_cffi.decompress_ptr(c, out_tmp_ptr)
+        dtoc = timer()
     assert((in_ == out).all())
     print("  Time for compress_ptr/decompress_ptr: %.3f/%.3f s." %
           (ctoc-ctic, dtoc-dtic), end='')
