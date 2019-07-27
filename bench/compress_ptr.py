11e4d68764cd0991aca89991fac7e7fc45d68236
Alecks Gates <agates@mail.agates.io>
2019-07-26 21:13:41 -0500

    Update compress_ptr to write results to a csv

--- bench/compress_ptr.py
+++ bench/compress_ptr.py
@@ -11,10 +11,12 @@ Small benchmark that compares a plain NumPy array copy against
 compression through different compressors in Blosc.
 """
 
-import numpy as np
+import csv
 from timeit import default_timer as timer
+
 import blosc_cffi
 from cffi import FFI
+import numpy as np
 
 ffi = FFI()
 
@@ -25,9 +27,9 @@ Nexp = np.log10(N)
 blosc_cffi.print_versions()
 
 print("Creating NumPy arrays with 10**%d int64/float64 elements:" % Nexp)
-arrays = ((np.arange(N, dtype=np.int64), "the arange linear distribution"),
-          (np.linspace(0, 1000, N), "the linspace linear distribution"),
-          (np.random.random_integers(0, 1000, N), "the random distribution")
+arrays = ((np.arange(N, dtype=np.int64), "arange linear distribution"),
+          (np.linspace(0, 1000, N), "linspace linear distribution"),
+          (np.random.random_integers(0, 1000, N), "random distribution")
           )
 
 in_ = arrays[0][0]
@@ -43,22 +45,32 @@ print("  *** ctypes.memmove() *** Time for memcpy():\t%.3f s\t(%.2f GB/s)" % (
 
 print("\nTimes for compressing/decompressing with clevel=%d and %d threads" % (
     clevel, blosc_cffi.ncores))
-for (in_, label) in arrays:
-    print("\n*** %s ***" % label)
-    for cname in blosc_cffi.compressor_list():
-        for filter in [blosc_cffi.NOSHUFFLE, blosc_cffi.SHUFFLE, blosc_cffi.BITSHUFFLE]:
-            with ffi.from_buffer(in_) as in_ptr:
-                t0 = timer()
-                c = blosc_cffi.compress_ptr(in_ptr,
-                                            in_.size, in_.dtype.itemsize,
-                                            clevel=clevel, shuffle=filter, cname=cname)
-                tc = timer() - t0
-            out = np.empty(in_.size, dtype=in_.dtype)
-            with ffi.from_buffer(out) as out_ptr:
-                t0 = timer()
-                blosc_cffi.decompress_ptr(c, out_ptr)
-                td = timer() - t0
-            assert((in_ == out).all())
-            print("  *** %-8s, %-10s *** %6.3f s (%.2f GB/s) / %5.3f s (%.2f GB/s)" % (
-                cname, blosc_cffi.filters[filter], tc, ((N * 8 / tc) / 2 ** 30), td, ((N * 8 / td) / 2 ** 30)), end='')
-            print("\tCompr. ratio: %5.1fx" % (N*8. / len(c)))
+with open("./results/compress_ptr.csv", "w") as results_file:
+    writer = csv.writer(results_file)
+    writer.writerow(("Array type", "Compressor", "Filter",
+                      "Compression Time (s)", "Compression Bandwidth (GB/s)",
+                      "Decompression Time (s)", "Decompression Bandwidth (GB/s)",
+                      "Compression Ratio"))
+    for (in_, label) in arrays:
+        print("\n*** %s ***" % label)
+        for cname in blosc_cffi.compressor_list():
+            for filter in [blosc_cffi.NOSHUFFLE, blosc_cffi.SHUFFLE, blosc_cffi.BITSHUFFLE]:
+                with ffi.from_buffer(in_) as in_ptr:
+                    t0 = timer()
+                    c = blosc_cffi.compress_ptr(in_ptr,
+                                                in_.size, in_.dtype.itemsize,
+                                                clevel=clevel, shuffle=filter, cname=cname)
+                    tc = timer() - t0
+                out = np.empty(in_.size, dtype=in_.dtype)
+                with ffi.from_buffer(out) as out_ptr:
+                    t0 = timer()
+                    blosc_cffi.decompress_ptr(c, out_ptr)
+                    td = timer() - t0
+                assert((in_ == out).all())
+                writer.writerow((label, cname, blosc_cffi.filters[filter],
+                                  tc, ((N * 8 / tc) / 2 ** 30),
+                                  td, ((N * 8 / td) / 2 ** 30),
+                                  N*8. / len(c)))
+                print("  *** %-8s, %-10s *** %6.3f s (%.2f GB/s) / %5.3f s (%.2f GB/s)" % (
+                    cname, blosc_cffi.filters[filter], tc, ((N * 8 / tc) / 2 ** 30), td, ((N * 8 / td) / 2 ** 30)), end='')
+                print("\tCompr. ratio: %5.1fx" % (N*8. / len(c)))
